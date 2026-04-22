1ff1defdc68da696ac5ead0cb632dcc2422c8c70
kudoshinichi2k5 <23520797@gm.uit.edu.vn>
2026-04-22 08:25:55 +0000

    feat: add logic code about module security

---
+++ modules/security/main.tf
@@ -0,0 +1,43 @@
+resource "aws_security_group" "sg" {
+  name        = "sg-${var.environment}"
+  description = "Allow SSH, HTTP, ICMP inbound traffic"
+  vpc_id      = module.network.vpc_id # tham chieu den output
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "All ICMP from anywhere"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "sg-${var.environment}"
+    Environment = var.environment
+    Project     = var.project_name
+    Owner       = var.owner
+  }
+}
---
+++ modules/security/variables.tf
@@ -0,0 +1,1 @@
+variable "mssv"   { type = string }
