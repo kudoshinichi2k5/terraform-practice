253475e306d817cdc3e22acda5e6bcb3dd614e48
kudoshinichi2k5 <23520797@gm.uit.edu.vn>
2026-04-22 04:16:07 +0000

    feat: add provider

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,10 +1,19 @@
 terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.44.0"
+    }
+  }
+
   backend "s3" {
-    bucket = "nt542-kienle"
-    key    = "dev/terraform.tfstate" # Đường dẫn lưu file state trong bucket
-    region = "us-east-1"
+    bucket = var.s3_bucket
+    key    = var.s3_key
+    region = var.aws_region
 
     # Khuyến nghị sau này: Thêm DynamoDB để khóa state (State Locking) khi làm việc nhóm
-    # dynamodb_table = "terraform-state-lock" 
+    # dynamodb_table = var.dynamodb_table
   }
 }
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,3 +1,17 @@
+variable "s3_bucket" {
+  type = string
+  description = "Tên S3 bucket để lưu trữ Terraform state."
+}
+
+variable "s3_key" {
+  type = string
+  description = "Đường dẫn file state trong bucket."
+}
+
+# variable "dynamodb_table" {
+#   type = string
+#   description = "Tên DynamoDB table để khóa state (nếu sử dụng)."
+# }
 variable "aws_region" {
   type = string
 }
